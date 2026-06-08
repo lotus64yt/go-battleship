4a8ed80afee9c72e0b6b08b81d91ae1812b90934
lotus64yt <lotus64santon@gmail.com>
2026-06-08 15:25:48 +0200

    fix: update goclic version

--- app/menu.go
+++ app/menu.go
@@ -11,18 +11,18 @@ func Menu() {
 	var action func()
 
 	m.Add(components.NewDisplay("BattleGO"))
-	m.Add(components.NewButton("Create a lobby", func() {
+	m.Add(components.NewButton("Create a lobby", components.WithButtonOnClick(func() {
 		action = func() { CreateGame() }
 		m.Stop()
-	}))
+	})))
 	m.Add(components.NewInput("Host IP:Port (Required)", &ip))
-	m.Add(components.NewButton("Join", func() {
+	m.Add(components.NewButton("Join", components.WithButtonOnClick(func() {
 		if ip == "" {
 			return
 		}
 		action = func() { JoinGame(ip) }
 		m.Stop()
-	}))
+	})))
 	m.Start()
 
 	if action != nil {
--- go.mod
+++ go.mod
@@ -2,11 +2,10 @@ module battleship
 
 go 1.25.0
 
-require github.com/octarahq/goclic v1.0.2
+require github.com/octarahq/goclic v1.1.0
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	golang.org/x/sys v0.44.0 // indirect
 	golang.org/x/term v0.43.0 // indirect
 )
-
-replace github.com/octarahq/goclic => /home/lotus/Documents/goclic

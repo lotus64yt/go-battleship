1be6d5fb6afa8a81636280389d502c95d49687f5
lotus64yt <lotus64santon@gmail.com>
2026-06-09 03:20:54 +0200

    feat: cache server port to reuse the same local port on restart

--- utils/conn/port.go
+++ utils/conn/port.go
@@ -1,15 +1,45 @@
 package conn
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func GetFreePort() (port int, err error) {
+const portCacheFile = ".battleship_port"
+
+func isPortFree(port int) bool {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return false
+	}
+	l.Close()
+	return true
+}
+
+func GetFreePort() (int, error) {
+	data, err := os.ReadFile(portCacheFile)
+	if err == nil {
+		portStr := strings.TrimSpace(string(data))
+		port, err := strconv.Atoi(portStr)
+		if err == nil && port > 0 && port <= 65535 {
+			if isPortFree(port) {
+				return port, nil
+			}
+		}
+	}
+
 	var a *net.TCPAddr
 	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
 		var l *net.TCPListener
 		if l, err = net.ListenTCP("tcp", a); err == nil {
-			defer l.Close()
-			return l.Addr().(*net.TCPAddr).Port, nil
+			port := l.Addr().(*net.TCPAddr).Port
+			l.Close()
+			os.WriteFile(portCacheFile, []byte(strconv.Itoa(port)), 0644)
+			return port, nil
 		}
 	}
-	return
+	return 0, err
 }
